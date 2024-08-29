9b5b8428bc9e0db741fd61fe31670d9aea4cdda5
Lou (loha) <loha@odoo.com>
2024-08-29 13:04:50 +0200

    🏴󠁧󠁢󠁥󠁮󠁧󠁿 fix cookie

--- static/translations/translation.mjs
+++ static/translations/translation.mjs
@@ -1,6 +1,6 @@
 export async function initTranslations(){
-    if(document.cookie.includes("lang=")){
-        global.settings.lang = document.cookie.split("lang=")[1].split(";")[0]
+    if(document.cookie.includes("disclone_lang=")){
+        global.settings.lang = document.cookie.split("disclone_lang=")[1].split(";")[0]
     }else{
         global.settings.lang = navigator.language.split("-")[0]
     }
@@ -17,7 +17,7 @@ export async function initTranslations(){
 }
 
 function setLang(code){
-    document.cookie = "lang=" + code + ";path=/"
+    document.cookie = "disclone_lang=" + code + ";path=/"
     document.location.reload()
 }
 window.setLang = setLang
