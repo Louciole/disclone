932a5b92c0c4e3d8620ad52d1096872dc3f7603b
Lou ! <lou@habert.me>
2024-09-04 03:19:13 +0200

    🕵️‍♀️🚂🚃🌅 reset password !

--- static/home/auth.js
+++ static/home/auth.js
@@ -52,6 +52,29 @@ function signupSubmit(event){
     event.preventDefault();
 }
 
+function resetSubmit(event){
+    const url = "/changePasswordVerif";
+    const errorBox = document.querySelector("#messageframe");
+
+    let request = new XMLHttpRequest();
+    request.open('POST', url, true);
+    request.onload = function() { // request successful
+        if (request.responseText === "ok"){
+            window.location.href = "/auth";
+        }else{
+            errorBox.innerHTML=request.responseText
+        }
+    };
+
+    request.onerror = function() {
+        console.log("request failed")
+    };
+
+    console.log(new FormData(event.target))
+    request.send(new FormData(event.target));
+    event.preventDefault();
+}
+
 function attachFormSubmitEvent(formId,fn){
     document.getElementById(formId).addEventListener("submit", fn);
 }
@@ -79,4 +102,40 @@ function resendValidation(){
 function activateBtn(btn){
     btn.style.color="grey"
     btn.onclick=resendValidation
+}
+
+function initReset(){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    if (urlParams.has('email')){
+        const mailBox =  document.getElementById("mail")
+        mailBox.value =  urlParams.get('email')
+        attachFormSubmitEvent('resetForm',resetSubmit)
+    }
+}
+
+function askReset(){
+    const errorBox = document.querySelector("#messageframe");
+    const mail = document.getElementById("email").value
+    if (mail === ""){
+        errorBox.innerHTML="Please enter your email to reset your password"
+        return
+    }
+    const url = "/passwordReset?email="+mail;
+
+    let request = new XMLHttpRequest();
+    request.open('POST', url, true);
+    request.onload = function() {
+        if (request.responseText === "ok"){
+            window.location.href = "/reset?email="+mail;
+        }else{
+            errorBox.innerHTML=request.responseText
+        }
+    };
+
+    request.onerror = function() {
+        console.log("request failed")
+    };
+    request.send();
 }
